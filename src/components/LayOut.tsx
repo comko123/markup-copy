726ebbac53a3eac97a5d62f599d156e08e2b29c9
Minseok Kim <comko123@naver.com>
2023-04-15 00:59:42 +0900

    차트페이지 반응형 조정

--- src/components/LayOut.tsx
+++ src/components/LayOut.tsx
@@ -98,14 +98,14 @@ onClick={()=>setModalState(state=>{
 {login?<footer className="relative">
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" 
 onClick={()=>setModalState(()=>{return{addToDoModalState:{...addToDoModalState,modalShowing:false},alertState:{alertShowing:!alertShowing}}})}
-className={`w-[3rem] aspect-square lg:w-[4%] ${alertShowing?"text-white":"text-black"} fixed bottom-[9%] right-[6%] hover:text-white cursor-pointer bg-blue-500 p-1 rounded-full shadow-md shadow-slate-400`}>
+className={`w-[3rem] aspect-square lg:w-[4%] ${alertShowing?"text-white":"text-black"} fixed bottom-[9%] right-[7em] hover:text-white cursor-pointer bg-blue-500 p-1 rounded-full shadow-md shadow-slate-400`}>
   <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z" />
 </svg>
 
 <AnimatePresence>
  {alertShowing?
   <motion.div variants={modalVariants} transition={modalVariants.transition} initial="start" animate="display" exit="end"
-  className="fixed right-[1.5%]  bottom-[8em] xl:bottom-[19vh] p-2 origin-bottom w-[13rem] md:w-[16rem] aspect-square my-2 bg-pink-100 rounded-xl">
+  className="fixed right-[3%]  bottom-[5em] xl:bottom-[19vh] p-2 origin-bottom w-[13rem] md:w-[16rem] aspect-square my-2 bg-pink-100 z-20 rounded-xl">
 {alertSample.length?<>
   <div className="text-slate-600 flex text-vxs font-bold w-full justify-end border-b-2 pb-1 border-sky-500"
 ><span onClick={()=>setAlertSample([])} className="hover:text-amber-600">모두 읽기</span></div>
