08f6a5bb97bb96e7bbeecada7efd2f4d07ae6af8
Minseok Kim <comko123@naver.com>
2023-04-14 17:57:05 +0900

    차트페이지 반응형 조정

--- src/graph_options/donutOptions.ts
+++ src/graph_options/donutOptions.ts
@@ -1,4 +1,4 @@
 export const donutOptions = {
-    plotOptions:{pie:{donut:{size:"50%",labels:{show:true,total:{show:true,fontWeight:800}}}}},
-chart:{height:50,width:50},legend:{fontWeight: 600}
+    plotOptions:{pie:{donut:{size:"50%",labels:{show:true,total:{show:true,fontWeight:600},}}}},
+chart:{height:50,width:50},legend:{show:false}
 }
