4170447a38e26f71f178039b73474686aba73d09
Minseok Kim <comko123@naver.com>
2023-04-17 18:00:39 +0900

    fix : 팔로워 페이지 모바일 ui 변경

--- src/pages/follower/index.tsx
+++ src/pages/follower/index.tsx
@@ -14,28 +14,31 @@ const Profil:NextPage = () => {
   const {login} =  useRecoilValue(loginAtom)
   useLoginCheck(login)
     return(<LayOut login={login}>
-      <div className="flex mx-14 my-10">
-       <nav className="border-r-2 border-gray-300 w-80 h-[75vh] pr-5">
-       <div className={`bg-gray-100 w-72 ${state.profilState?"h-80":"h-96"} rounded-xl pt-2 relative shadow-xl`}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
+      <main className="flex flex-col lg:flex-row mx-14 my-10">
+       <nav className="border-b-2 lg:border-r-2 lg:border-b-transparent border-gray-300 w-full lg:w-[32%] h-max pr-5">
+       <div className={`bg-gray-200 pt-7 lg:pt-0 w-[110%] md:w-full lg:w-[95%] ${state.profilState?"h-52 lg:h-80 ":"pt-10 h-60 lg:h-[25rem]"} flex justify-center lg:flex-col rounded-xl px-2 relative shadow-xl`}>
+       <div className="h-[35%] mt-4 lg:mt-0">
+       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
         strokeWidth={1.5} stroke="currentColor" 
-        className="w-28 aspect-square bg-white rounded-full mt-8 mx-auto p-1">
+        className="w-28 aspect-square bg-white rounded-full mx-auto p-1">
   <path strokeLinecap="round" strokeLinejoin="round" 
   d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
 </svg>
 
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" 
-className="w-6 aspect-square bg-white absolute right-[6rem] rounded-full border-2 border-black top-[7.8rem]">
+className="w-6 aspect-square bg-white relative left-[5.5em] lg:left-[60%] -top-[2rem] rounded-full border-2 border-black">
   <path strokeLinecap="round" strokeLinejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z" />
   <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
 </svg>
 
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" 
-className="w-6 h-6 bg-white absolute left-[6rem] rounded-full border-2 border-black top-[7.8rem]">
+className="w-6 h-6 bg-white relative left-[0.1rem] lg:left-[32%] -top-[3.48rem] rounded-full border-2 border-black">
   <path strokeLinecap="round" strokeLinejoin="round" d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
   <path strokeLinecap="round" strokeLinejoin="round" d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z" />
 </svg>
+       </div>
 
+<div className={`flex flex-col ${state.profilState?null:"-mt-6 lg:-ml-7 xl:ml-0"}`}>
 {state.profilState?
     <div className="flex mt-3 font-semibold ml-8 text-sm max-w-xl"> 
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} 
@@ -45,7 +48,7 @@ stroke="currentColor" className="w-5 aspect-square mt-[0.2rem]">
 <span className="ml-1 w-full overflow-hidden text-ellipsis whitespace-nowrap">Lee Jinlee</span>
 </div>
 :<input defaultValue="Lee Jinlee"
-className="ml-8 mt-3 w-56 rounded-lg p-1 font-bold border-2 text-sm border-blue-500 outline-none "/>}
+className="ml-8 mt-3 w-52 xl:w-56  rounded-lg p-1 font-bold border-2 text-sm border-blue-500 outline-none "/>}
 
 {state.profilState?
     <div className="flex mt-3 font-semibold ml-8 text-sm max-w-xl"> 
@@ -55,7 +58,7 @@ stroke="currentColor" className="w-5 aspect-square mt-[0.2rem]">
 </svg>
 <div className="ml-1 w-full overflow-hidden text-ellipsis whitespace-nowrap">Hi, i&apos;m Devops engineer</div>
 </div>:<input defaultValue="Hi, i'm Devops engineer"
-className="ml-8 mt-3 w-56 rounded-lg p-1 font-bold border-2 text-sm border-blue-500 outline-none "/>}
+className="ml-8 mt-3 w-52 xl:w-56  rounded-lg p-1 font-bold border-2 text-sm border-blue-500 outline-none "/>}
 
 {state.profilState?
     <div className="flex mt-3 font-semibold ml-8 text-md max-w-xl">
@@ -65,7 +68,7 @@ className="ml-8 mt-3 w-56 rounded-lg p-1 font-bold border-2 text-sm border-blue-
 </svg>
 <span className="ml-1 w-full overflow-hidden text-ellipsis whitespace-nowrap">Hanshin Univ</span>
 </div>:<input defaultValue="Hanshin Univ"
-className="ml-8 mt-3 w-56 rounded-lg p-1 font-bold border-2 text-sm border-blue-500 outline-none "/>}
+className="ml-8 mt-3 w-52 xl:w-56  rounded-lg p-1 font-bold border-2 text-sm border-blue-500 outline-none "/>}
 
 {state.profilState?
     <div className="flex mt-3 font-semibold ml-8 text-sm max-w-xl"> 
@@ -75,21 +78,24 @@ className="ml-8 mt-3 w-56 rounded-lg p-1 font-bold border-2 text-sm border-blue-
 </svg>
 <span className="ml-1 w-full overflow-hidden text-ellipsis whitespace-nowrap">leejinlee.kr@gmail.com</span>
 </div>:<input defaultValue="leejinlee.kr@gmail.com" 
-className="ml-8 mt-3 w-56 rounded-lg p-1 font-bold border-2 text-sm border-blue-500 outline-none "/>}
+className="ml-8 mt-3 w-52 xl:w-56  rounded-lg p-1 font-bold border-2 text-sm border-blue-500 outline-none "/>}
 
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" onClick={()=>setState(state=>{return {...state , profilState:!state.profilState}})} 
 viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" 
-className={`w-6 h-6 bg-white border-2 border-black rounded-full absolute ${state.profilState?"bottom-7":"bottom-3"} right-5 cursor-pointer`}>
+className={`w-6 h-6 bg-white border-2 border-black rounded-full absolute ${state.profilState?"bottom-4":"bottom-3"} right-5 cursor-pointer`}>
   <path strokeLinecap="round" strokeLinejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z" />
   <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
 </svg>
+
+</div>
 </div> 
 
- <div className="font-bold text-center w-72 mt-7">
+ <div className="font-bold text-center w-full  my-7">
   <button className="shadow-lg border-2 border-blue-500 text-blue-500 p-2 rounded-xl hover:bg-blue-500 hover:text-white">회원 탈퇴</button></div>
        </nav>
-<aside className="overflow-x-hidden pt-3 w-[80%]">
-    <div className="flex font-bold justify-between px-8 w-full text-lg ml-9 border-b-2 border-gray-300 pb-3">
+
+<aside className=" pt-3 w-full">
+    <div className="flex font-bold justify-between px-8 w-full text-lg lg:ml-9 border-b-2 border-gray-300 pb-3">
         <div className={`mx-auto ${state.follower?"ring-4 ring-blue-500 ring-offset-3":null} text-lg p-2 rounded-lg`}
         onClick={()=>setState(state=>{return{...state,follower:true,request:false}})}
         >Follower</div>
@@ -99,7 +105,7 @@ className={`w-6 h-6 bg-white border-2 border-black rounded-full absolute ${state
         </div>
         </div>
     <form onSubmit={e=>e.preventDefault()} className="relative">
-        <input type="text" className="w-[90%] shadow-lg p-2 font-bold bg-gray-100 mt-5 ml-9 rounded-lg outline-none"/>
+        <input type="text" className="w-[90%] shadow-lg p-2 font-bold bg-gray-100 mt-5 lg:ml-9 rounded-lg outline-none"/>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} 
         stroke="currentColor" className="w-6 h-6 absolute top-6 right-[8%] mt-1">
   <path strokeLinecap="round" strokeLinejoin="round" 
@@ -112,21 +118,21 @@ className={`w-6 h-6 bg-white border-2 border-black rounded-full absolute ${state
   <div className="max-h-[60vh] overflow-auto mt-5 scrollbar-hide">
   {Array(5).fill({avatar:"",name:"follower",
    timeLine:"Hi, i'm Devops engineer follower.",follow:true}).map((item,index)=>{return(
-   <div key={index} className="my-5 ml-9 bg-gray-100 rounded-xl flex h-32 w-[90%] p-4 shadow-lg">
-       {item.avatar?
+   <div key={index} className="my-5 lg:ml-9 bg-gray-100 rounded-xl flex h-32 w-[99%] lg:w-[90%] p-4 shadow-lg">
+       <div className="flex items-center justify-center">{item.avatar?
        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" 
-       className="w-20 aspect-square p-1 bg-white rounded-full my-auto mr-4">
+       className="w-16 lg:w-20 aspect-square p-1 bg-white rounded-full mr-4">
        <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
      </svg>:
      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" 
-     className="w-20 aspect-square p-1 bg-white rounded-full my-auto mr-4">
+     className="w-16 lg:w-20 aspect-square p-1 bg-white rounded-full mr-4">
  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
 </svg>
-}
+}</div>
 <div className="border-l-4 border-gray-400 pl-5 flex w-full justify-between relative">
-<div className=" pt-3 text-lg max-w-xs">
-<div className="font-bold w-full overflow-hidden text-ellipsis whitespace-nowrap">{item.name}</div>
-<div className="pt-4 font-bold w-full overflow-hidden text-ellipsis whitespace-nowrap">{item.timeLine}</div>
+<div className=" pt-3 max-w-xs">
+<div className="font-bold w-[70%] lg:w-full overflow-hidden text-ellipsis whitespace-nowrap">{item.name}</div>
+<div className="pt-4 font-bold w-[70%] lg:w-full overflow-hidden text-ellipsis whitespace-nowrap">{item.timeLine}</div>
 </div>
 <input type="button" value={item.follow?"follow":"unfollow"} 
 className={`border-2 rounded-lg text-sm h-12 px-2 font-bold relative top-5 right-4 bg-white cursor-pointer 
@@ -164,7 +170,7 @@ ${item.follow?"border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white"
 
 }
 </aside>
-    </div>
+    </main>
     </LayOut>)
 }
 export default Profil
