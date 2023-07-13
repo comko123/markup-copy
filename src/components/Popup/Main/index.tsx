2f5a79ce6ffa5d2541d85c1bea185a68142a0ac7
Minseok Kim <comko123@naver.com>
2023-07-14 00:21:01 +0900

    fix : 팝업 애니메이션 수정

--- src/components/Popup/Main/index.tsx
+++ src/components/Popup/Main/index.tsx
@@ -18,12 +18,13 @@ useEffect(()=>{
   return()=>{setPopupModify(null)}
 },[])
 
+
 return(
 <motion.div variants={popUpVariants} initial="start" animate="display" exit="end" className="z-20">
 <motion.div onClick={()=>setPuState(state=>!state)}
 className="bg-gray-900 w-full h-full fixed top-0 left-0 opacity-50 font-bold"/>
-<motion.div
-className="fixed top-[12%] left-[5%] xl:top-[8%] xl:left-[20%] md:left-[12%] w-[90%] md:w-[78%] xl:w-[60%] h-[75%] xl:h-[85%] bg-white rounded-xl pb-5 overflow-auto scrollbar-hide">
+<motion.div exit={{y:[8,5,0,-10,-30],transition:{ease:"linear"}}}
+className=" fixed top-[12%] left-[5%] xl:top-[8%] xl:left-[20%] md:left-[12%] w-[90%] md:w-[78%] xl:w-[60%] h-[75%] xl:h-[85%] bg-white rounded-xl pb-5 overflow-auto scrollbar-hide">
 <header className="bg-gray-300  h-[4em] md:h-[5em] rounded-t-xl flex justify-end px-3 pt-3 w-full">
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor"
  className="w-6 h-6 md:w-10 md:h-10 hover:text-red-500" onClick = {()=>setPuState(state=>!state)}>
