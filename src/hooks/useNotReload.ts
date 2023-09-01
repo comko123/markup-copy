7e7fdef2f11ec5478d8da220b75698dc1d1e39de
Minseok Kim <comko123@naver.com>
2023-09-02 00:56:36 +0900

    Refactor : 팔로워 애니메이션 추가및 기능 수정.

--- src/hooks/useNotReload.ts
+++ src/hooks/useNotReload.ts
@@ -6,9 +6,8 @@ export const preventClose = (e: BeforeUnloadEvent) => {
   // chrome에서는 설정이 필요해서 넣은 코드
 }
 export const useNotReload = () => {
-  const warring = window.addEventListener("beforeunload", preventClose)
   useEffect(() => {
-    void (() => warring)()
-    return () => warring
-  }, [warring])
+    void (() => window.addEventListener("beforeunload", preventClose))()
+    return () => window.addEventListener("beforeunload", preventClose)
+  }, [])
 }
